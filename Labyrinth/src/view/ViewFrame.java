e802276f30495c3dbf3db6cac2fcd4e0d889537a
rembrisset <remi.brisset@etu.u-bordeaux.fr>
2017-11-27 18:36:30 +0100

    debut view

---
+++ Labyrinth/src/view/ViewFrame.java
@@ -0,0 +1,19 @@
+package view;
+
+import javafx.stage.*;
+import javafx.scene.paint.*;
+
+public class ViewFrame {
+	
+	static final int SPAN = 4;
+	static final int WALL = 2;
+	static final int CELL = 9;
+	public static final Paint WALL_COLOR = Color.BURLYWOOD;
+	public static final Paint BG_COLOR = Color.CORNSILK;
+	
+	
+	public static void drawFrame(Stage stage, int nbX, int nbY) {
+		
+	}
+	
+}
