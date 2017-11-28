6168b6beb5abf5856fb81456bb0cd974bde49560
rembrisset <remi.brisset@etu.u-bordeaux.fr>
2017-11-28 13:38:57 +0100

    main

--- Labyrinth/src/main/Main.java
+++ Labyrinth/src/main/Main.java
@@ -6,7 +6,6 @@ import model.Labyrinth;
 import view.ViewFrame;
 
 
-
 public class Main extends Application{
 
 	public static void main(String[] args) {
@@ -16,8 +15,6 @@ public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		primaryStage.setWidth(1024);
-		primaryStage.setHeight(968);
 		primaryStage.setTitle("Labyrinth");
 		ViewFrame.drawFrame(primaryStage, 16, 16);
 		primaryStage.show();
