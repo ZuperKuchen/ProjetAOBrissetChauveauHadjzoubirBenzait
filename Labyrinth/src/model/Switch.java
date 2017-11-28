c4558aedd82f617f7f50ea1f0304f18e98a7c24a
PierreChauveau <chauveau.pierre@live.com>
2017-11-28 16:28:45 +0100

    fr

--- Labyrinth/src/model/Switch.java
+++ Labyrinth/src/model/Switch.java
@@ -3,10 +3,11 @@ package model;
 import org.jgrapht.Graph;
 
 public class Switch extends Fixed {
+	
+	
 
-	public Switch(int x, int y, Graph<Cell, Edge> graph, Cell[][] cellArray, boolean on) {
+	public Switch(int x, int y, Graph<Cell, Edge> graph, Cell[][] cellArray, boolean on, Edge door) {
 		super(x, y, graph, cellArray, on);
-		// TODO Auto-generated constructor stub
 	}
 
 }
