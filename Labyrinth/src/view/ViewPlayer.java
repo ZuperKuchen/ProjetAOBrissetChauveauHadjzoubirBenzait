c323b89c31736363cd529530c5d850a6fe391ac4
rembrisset <remi.brisset@etu.u-bordeaux.fr>
2017-11-28 16:32:44 +0100

    viewplayer

---
+++ Labyrinth/src/view/ViewPlayer.java
@@ -0,0 +1,29 @@
+package view;
+
+
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import model.Player;
+
+public class ViewPlayer{
+	private static String imgPath = "../img/player.png";
+	private static ImageView imageView;
+	private static Player player = Player.getInstance();
+	
+	
+	
+	//NOT FINISH
+	public static void View(){
+		Image image = new Image(imgPath);
+		imageView = new ImageView(image);
+		
+		ViewFrame.pane.getChildren().add(ViewPlayer.imageView);
+		double xt = (int) ((ViewFrame.WALL + player.getX() * (ViewFrame.WALL + ViewFrame.CELL)) * ViewFrame.SPAN);
+		double yt = (int) ((ViewFrame.WALL + player.getY() * (ViewFrame.WALL + ViewFrame.CELL)) * ViewFrame.SPAN);
+		imageView.setX(xt);
+		imageView.setY(yt);
+	}
+
+	
+}
