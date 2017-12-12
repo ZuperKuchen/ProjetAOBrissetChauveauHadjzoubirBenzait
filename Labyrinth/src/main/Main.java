ee482e2d0da231a9ddd5bd177ed0c1f071a033fd
PierreChauveau <chauveau.pierre@live.com>
2017-12-12 13:35:59 +0100

    maj

--- Labyrinth/src/main/Main.java
+++ Labyrinth/src/main/Main.java
@@ -2,8 +2,6 @@ package main;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import view.*;
-import model.*;
 import controller.RunGame;
 
 public class Main extends Application{
