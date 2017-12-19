479abecd3b1e7c04b94b0804c2974dfa2e9f9a25
PierreChauveau <chauveau.pierre@live.com>
2017-12-19 16:32:29 +0100

    ajout fonction deplacement

--- Labyrinth/src/model/Monster.java
+++ Labyrinth/src/model/Monster.java
@@ -6,4 +6,11 @@ public class Monster extends Item {
 	public Monster(int x, int y) {
 		super(x, y);
 	}
+	
+	public Cell nextCell(Labyrinth lab) {
+		
+		
+		Cell cell= new Cell(1,2);
+		return cell;
+	}
 }
