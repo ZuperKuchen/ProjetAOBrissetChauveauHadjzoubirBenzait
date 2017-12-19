e6f460b02fc44471ef54cb334403797983e91ec0
PierreChauveau <chauveau.pierre@live.com>
2017-12-19 17:31:03 +0100

    on retire la méthode inutile

--- Labyrinth/src/model/Monster.java
+++ Labyrinth/src/model/Monster.java
@@ -6,11 +6,4 @@ public class Monster extends Item {
 	public Monster(int x, int y) {
 		super(x, y);
 	}
-	
-	public Cell nextCell(Labyrinth lab) {
-		
-		
-		Cell cell= new Cell(1,2);
-		return cell;
-	}
 }
