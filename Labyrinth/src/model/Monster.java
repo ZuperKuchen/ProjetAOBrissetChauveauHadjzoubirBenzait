4daba71ba8908bc2d62ffbe0394ad6a0c66fbf2a
PierreChauveau <chauveau.pierre@live.com>
2017-12-20 16:13:03 +0100

    le monstre n'est plus fantomatique

--- Labyrinth/src/model/Monster.java
+++ Labyrinth/src/model/Monster.java
@@ -18,22 +18,26 @@ public class Monster extends Item {
 		int mark = monsterCell.getMark();
 		for(Direction dir : Direction.values()) {
 			if(dir == Direction.NORTH) {
-				if (this.getY() > 0 && cellArray[this.getX()][this.getY() - 1].getMark() == mark - 1) {
+				if (this.getY() > 0 && cellArray[this.getX()][this.getY() - 1].getMark() == mark - 1 
+						&& currentLvl.getLab().mayGo(dir, monsterCell) != null) {
 					this.setY(this.getY() - 1);
 				}
 			}
 			if(dir == Direction.SOUTH) {
-				if (this.getY() < currentLvl.getLab().getSizeY() - 1 && cellArray[this.getX()][this.getY() + 1].getMark() == mark - 1) {
+				if (this.getY() < currentLvl.getLab().getSizeY() - 1 && cellArray[this.getX()][this.getY() + 1].getMark() == mark - 1
+						&& currentLvl.getLab().mayGo(dir, monsterCell) != null) {
 					this.setY(this.getY() + 1);
 				}
 			}
 			if(dir == Direction.EAST) {
-				if (this.getX() < currentLvl.getLab().getSizeX() - 1 && cellArray[this.getX() + 1][this.getY()].getMark() == mark - 1) {
+				if (this.getX() < currentLvl.getLab().getSizeX() - 1 && cellArray[this.getX() + 1][this.getY()].getMark() == mark - 1
+						&& currentLvl.getLab().mayGo(dir, monsterCell) != null) {
 					this.setX(this.getX() + 1);
 				}
 			}
 			if(dir == Direction.WEST) {
-				if (this.getX() > 0 && cellArray[this.getX() - 1][this.getY()].getMark() == mark - 1) {
+				if (this.getX() > 0 && cellArray[this.getX() - 1][this.getY()].getMark() == mark - 1
+						&& currentLvl.getLab().mayGo(dir, monsterCell) != null) {
 					this.setX(this.getX() - 1);
 				}
 			}
